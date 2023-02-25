f869508471e7b8d6032b8adc49585d34d2618a2b
Tanhoang845 <84836379+Tanhoang845@users.noreply.github.com>
2023-02-25 22:23:57 +0700

    Devide Header, Footer

---
+++ source/web_sachtruyen/header.php
@@ -0,0 +1,68 @@
+<header>
+        <div class="row header">
+            <!-- Logo -->
+            <div class="col-xs-4 col-sm-4 col-md-2 col-lg-2 logo">
+                <img src="images/logo/main-logo.jpg" alt="main-logo">
+            </div>
+            <!-- Search -->
+            <div class="col-md-6"> 
+                <div class="search-box mt-4"> 
+                    <div class="row">
+                        <form class="search-form" action="#"> 
+                            <input class="form-control input col-5" placeholder="Nhập tên sách, tác giả.." type="text"> 
+                            <button class="btn btn-link search-btn col-1"> 
+                                <i class="bi bi-search"></i> 
+                            </button> 
+                        </form> 
+                    </div>
+                </div> 
+            </div> 
+            <!-- Đăng nhập -->
+            <div class="header-button col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="row">
+                    <div class=" col mt-4">
+                        <a href="#" class="upgrade-author">
+                            <img src="images/header/upgrade-author.png" alt="">
+                            Trở thành tác giả
+                        </a>
+                    </div>
+                    <div class="col mt-4">
+                        <a href="" class="header-signin">
+                            <img src="images/header/icon-user.svg" alt="">
+                            Đăng nhập
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Nav bar -->
+        <div class="header-nav">
+            <ul class="nav nav-tabs border border-success border-end-0 border-bottom-0 border-start-0 border-2">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Thể loại</a>
+                        <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Truyện dài</a></li>
+                        <li><a class="dropdown-item" href="#">Truyện ngắn</a></li>
+                        <li><a class="dropdown-item" href="#">Truyện chữa lành</a></li>
+                        <!-- <li><hr class="dropdown-divider"></li> -->
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index1.php">Tất cả các truyện</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Truyện dài</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Truyện ngắn</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Truyện chữa lành</a>
+                </li>
+            </ul>
+        </div>
+    </header>
--- source/web_sachtruyen/index.php
+++ source/web_sachtruyen/index.php
@@ -17,74 +17,9 @@
 </head>
 
 <body>
-    <header>
-        <div class="row header">
-            <!-- Logo -->
-            <div class="col-xs-4 col-sm-4 col-md-2 col-lg-2 logo">
-                <img src="images/logo/main-logo.jpg" alt="main-logo">
-            </div>
-            <!-- Search -->
-            <div class="col-md-6"> 
-                <div class="search-box mt-4"> 
-                    <div class="row">
-                        <form class="search-form" action="#"> 
-                            <input class="form-control input col-5" placeholder="Nhập tên sách, tác giả.." type="text"> 
-                            <button class="btn btn-link search-btn col-1"> 
-                                <i class="bi bi-search"></i> 
-                            </button> 
-                        </form> 
-                    </div>
-                </div> 
-            </div> 
-            <!-- Đăng nhập -->
-            <div class="header-button col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                <div class="row">
-                    <div class=" col mt-4">
-                        <a href="#" class="upgrade-author">
-                            <img src="images/header/upgrade-author.png" alt="">
-                            Trở thành tác giả
-                        </a>
-                    </div>
-                    <div class="col mt-4">
-                        <a href="" class="header-signin">
-                            <img src="images/header/icon-user.svg" alt="">
-                            Đăng nhập
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Nav bar -->
-        <div class="header-nav">
-            <ul class="nav nav-tabs border border-success border-end-0 border-bottom-0 border-start-0 border-2">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Thể loại</a>
-                        <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Truyện dài</a></li>
-                        <li><a class="dropdown-item" href="#">Truyện ngắn</a></li>
-                        <li><a class="dropdown-item" href="#">Truyện chữa lành</a></li>
-                        <!-- <li><hr class="dropdown-divider"></li> -->
-                    </ul>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="index1.php">Tất cả các truyện</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Truyện dài</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Truyện ngắn</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Truyện chữa lành</a>
-                </li>
-            </ul>
-        </div>
-    </header>
+    <?php 
+        include("header.php");
+    ?>
 
     <!-- Body -->
     <div class="body">
@@ -205,65 +140,9 @@
     </div>
 
     <!-- Footer -->
-    <footer class="mt-5 mt-5 bg-light border border-success border-end-0 border-bottom-0 border-start-0 border-5">
-        <div class="row .text-secondary-emphasis">
-            <div class="col-6">
-                <div class="col-xs-4 col-sm-4 col-md-2 col-lg-2 logo">
-                    <img src="images/logo/main-logo.jpg" alt="main-logo">
-                </div>
-                <div class="font-weight-medium">
-                    Cơ quan chủ quản: <span class="fw-bolder">Tanhoang845 Development</span><br>
-                    Địa chỉ: D5-305 HUST, Việt Nam <br> 
-                    Email: tanhoang845@gmail.com | Tel: 023456789
-                </div>
-            </div>
-            <!-- Huu Ich -->
-            <div class="col-2">
-                <div class="footer-right footer-menu-list-wrap mt-5">
-                    <div class="title fw-bold text-center">HỮU ÍCH</div>
-                    <div class="content">
-                        <ul class="footer-menu-list">
-                            <li class="footer-menu-list-item">Chính sách bảo mật</li> 
-                            <li class="footer-menu-list-item">Điều khoản sử dụng</li> 
-                            <li class="footer-menu-list-item">
-                                <a href="" class="">
-                                    Quy định về nội dung
-                                </a>
-                            </li> 
-                            <li class="footer-menu-list-item">Vấn đề bản quyền</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <!-- Lien he -->
-            <div class="col-2">
-                <div class="footer-right footer-social-contact mt-5">
-                    <div class="title fw-bold">Liên hệ</div> 
-                    <div class="content">
-                        <div class="button-facebook-social">
-                            <a class="text-center" href="https://www.facebook.com/mangvanhocso1/" target="_blank">
-                                <!-- <img src="/cdv-assets/img/IconFacebook.ac66021.svg" class="icon-face"> -->
-                                Fanpage Cộng đồng
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- QR -->
-            <div class="col-2 mt-5">
-                <div class="el-col el-col-4" style="padding-left:15px;padding-right:15px;">
-                    <div class="footer-right footer-app-contact">
-                        <div class="title fw-bold">Tải app</div> 
-                        <div class="content">
-                            <div class="title-qr-app">Quét mã QR để cài APP</div> 
-                        <div class="qr-code-app">
-                            <!-- <img src="/cdv-assets/QR.png?v=1677035874671" class="lazyLoad isLoading"> -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php 
+        include("footer.php");
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 </body>
